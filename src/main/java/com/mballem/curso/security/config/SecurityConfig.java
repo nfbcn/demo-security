55b99bab616a83d7806dd7fa8224cc371a9ba920
Nikolas <nikolas.nascimento@neurotech.com.br>
2021-11-25 00:20:24 -0300

    Primeiros passos Spring Security 5

--- src/main/java/com/mballem/curso/security/config/SecurityConfig.java
+++ src/main/java/com/mballem/curso/security/config/SecurityConfig.java
@@ -1,20 +1,47 @@
 package com.mballem.curso.security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.mballem.curso.security.service.UsuarioService;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private UsuarioService service;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		// TODO Auto-generated method stub
 		
-		http.authorizeRequests().
-		antMatchers("/webjars/**", "/css/**", "/image/**","/js/**").permitAll().
-		antMatchers("/", "/home").permitAll().
-		anyRequest().authenticated();
+		http.authorizeRequests()
+		// acessos públicos liberados
+		.antMatchers("/webjars/**", "/css/**", "/image/**","/js/**").permitAll()
+		.antMatchers("/", "/home").permitAll()
+		.anyRequest().authenticated()
+		.and() //Métodos para fazer a autenticação 
+			.formLogin()
+			.loginPage("/login")
+			.defaultSuccessUrl("/", true)
+			.failureUrl("/login-error")
+			.permitAll()
+			.and()
+				.logout()
+				.logoutSuccessUrl("/");
+	
+			
+	}
+
+	//Método para comparar a senha do banco com a senha do formulário de login
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		//Criptografia BcryptPasswordEncoder é a recomendada pelo Framework
+		auth.userDetailsService(service).passwordEncoder(new BCryptPasswordEncoder());
 	}
 
 	
